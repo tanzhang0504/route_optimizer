c751b8c12e6c7b6c9acf937dcef1276980715e8a
tanzhang0504 <tanzhang0504@gmail.com>
2016-02-21 19:17:09 -0600

    Modify run_R_codes.sh to take parameters and delete route output files

--- run_R_codes.sh
+++ run_R_codes.sh
@@ -1,8 +1,11 @@
-Rscript get_assignment.R 0 0 bs_channel_data1.txt thruput_example1.txt tmpout.txt
-cat tmpout.txt
+#!/bin/bash
+# Example usage: ./run_R_codes.sh fairness_mode[0/1/4] topology[2_2/2_3/3_2]
 
-Rscript get_assignment.R 1 0 bs_channel_data1.txt thruput_example1.txt tmpout.txt
-cat tmpout.txt
+Rscript get_assignment.R 0 $1 conflict_$2.dat stats_$2.dat route_0.dat
+cat route_0.dat
 
-Rscript get_assignment.R 4 0 bs_channel_data1.txt thruput_example1.txt tmpout.txt
-cat tmpout.txt
+Rscript get_assignment.R 1 $1 conflict_$2.dat stats_$2.dat route_1.dat
+cat route_1.dat
+
+Rscript get_assignment.R 4 $1 conflict_$2.dat stats_$2.dat route_4.dat
+cat route_4.dat
